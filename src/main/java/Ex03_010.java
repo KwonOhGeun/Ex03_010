ae3b57423858ff98997f71fe89995c9f09a899eb
KwonOhGeun <101384390+KwonOhGeun@users.noreply.github.com>
2022-03-20 03:06:49 +0900

    Initial commit

---
+++ src/main/java/Ex03_010.java
@@ -0,0 +1,11 @@
+public class Ex03_010 {
+
+    public static void main(String[] args) {
+        int a = 100, b = 200;
+        float result;
+
+        result = a/b;
+
+        System.out.printf("%f \n", result );
+    }
+}
